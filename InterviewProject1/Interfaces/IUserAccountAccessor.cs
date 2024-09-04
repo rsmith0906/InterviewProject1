25e73f01ee2e5f31fd1d44ab97ec8984cb02bd67
Russell Smith (Valorem Consulting) <v-rusmit@microsoft.com>
2024-09-04 09:14:41 -0500

    Adding descriptions.

--- InterviewProject1/Interfaces/IUserAccountAccessor.cs
+++ InterviewProject1/Interfaces/IUserAccountAccessor.cs
@@ -2,10 +2,29 @@
 
 namespace InterviewProject1.Interfaces
 {
+    /// <summary>
+    /// The user account accessor.
+    /// </summary>
     public interface IUserAccountAccessor
     {
+        /// <summary>
+        /// The add deposit.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         Task AddDeposit(double amount);
+
+        /// <summary>
+        /// The get account history.
+        /// </summary>
+        /// <returns></returns>
         Task<List<AccountTransaction>> GetAccountHistory();
+
+        /// <summary>
+        /// The get balance.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         Task WithdrawMoney(double amount);
     }
 }
